17d3aa1ab6433958916ceb42f2dd38f01ebb9a38
= <=>
2024-07-03 20:28:55 +0530

    Initial Commit

---
+++ controller/UserController.js
@@ -0,0 +1,30 @@
+const userSchema = require('../model/UserSchema');
+const bcrypt = require('bcrypt');
+const salt = 10;
+
+const register = (req,resp) => {
+    bcrypt.hash(req.body.password,salt,function(err,hash){
+        if (err){
+            return resp.status(500).json(err);
+        }
+        const user = new userSchema({
+            fullName:req.body.fullName,
+            password:hash,
+            email:req.body.email,
+            activeState:req.body.activeState,
+        });
+        user.save().then(saveResponse=>{
+            return resp.status(201).json({'message':'Saved'});
+        }).catch(error=>{
+            return resp.status(500).json(error);
+        });
+    })
+}
+
+const login = (req,resp) => {
+    
+}
+
+module.exports={
+    register,login
+}
